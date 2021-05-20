a10e0b4d8156a232d18996b1ef036f03603e5dc1
Jack1q <johndonofrio2021@students.sjcadets.org>
2021-05-19 21:46:31 -0400

    updated a lot of stuff

---
+++ database.py
@@ -0,0 +1,25 @@
+import json
+
+class Database:
+    """ Class containing functions for handling position data."""
+    @staticmethod
+    def get_json():
+        return json.load(open('positions.json'))
+    @staticmethod
+    def get_stock_data(ticker):
+        return get_json()['ticker']
+    @staticmethod
+    def update_stock_data(ticker, share_delta, value):
+        data = Database.get_json()
+        if ticker in data.keys():
+            data[ticker] = {
+                "shares" : data[ticker]['shares'] + share_delta,
+                "value" : value
+            }
+        else:
+            data[ticker] = {
+                "shares" : share_delta,
+                "value" : value
+            }
+        with open('positions.json', 'w') as file:
+            json.dump(data, file)
--- main.py
+++ main.py
@@ -1,18 +1,94 @@
-import os
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from stock import StockData
+from database import Database
 
-def get_new_browser_window():
-    options = Options()
-    options.add_argument('user-data-dir=/Users/jackdonofrio/Library/Application Support/Google/Chrome/Profile 1')
-    browser = webdriver.Chrome(executable_path='/Users/jackdonofrio/Desktop/trade-bot/chromedriver', chrome_options=options)
-    return browser
-def close_browser(browser):
-    browser.close()
+class BotActions:
 
-browser = get_new_browser_window()
-browser.get('https://www.marketwatch.com/game/pool-to-the-moon-20')
+    @staticmethod
+    def open_new_browser_window():
+        options = Options()
+        options.add_argument('user-data-dir=C:/Users/jackd/AppData/Local/Google/Chrome/User Data/Profile 2')
+        browser = webdriver.Chrome(executable_path='C:/Users/jackd/OneDrive/Desktop/stock-trading/chromedriver.exe', chrome_options=options)
+        return browser
 
-browser.find_element_by_xpath('/html/body/div[5]/div[3]/div[1]/div[1]/div/div[1]/input').send_keys('GME') # input box
+    @staticmethod
+    def short_stock(browser, ticker, shares):
+        try:
+            browser.get('https://www.marketwatch.com/game/pool-to-the-moon-20')
+            browser.find_element_by_xpath('/html/body/div[5]/div[3]/div[1]/div[1]/div/div[1]/input').send_keys(ticker)
+            sleep(3)
+            browser.find_element_by_xpath('/html/body/div[5]/div[3]/div[1]/div[1]/div/div[2]/table/tbody/tr[1]/td[4]/button').click()
+            sleep(2)
+            price_at_trade_time = StockData(ticker).current_price
+            browser.find_element_by_xpath('/html/body/div[8]/div/div/div[1]/form/div[1]/ul/li[2]').click()
+            browser.find_element_by_xpath('/html/body/div[8]/div/div/div[1]/form/div[2]/div[2]/div[1]/input').send_keys(shares)
+            browser.find_element_by_xpath('/html/body/div[8]/div/div/div[1]/form/div[3]/div/button[3]').click()
+            # Update position in database
+            Database.update_stock_data(ticker, shares, price_at_trade_time)
+            print('Position updated')
+
+        except:
+            print(f'unable to short {shares} shares of {ticker}')
+
+    @staticmethod
+    def cover_stock(browser, ticker, shares):
+        try:
+            browser.get('https://www.marketwatch.com/game/pool-to-the-moon-20')
+            browser.find_element_by_xpath('/html/body/div[5]/div[3]/div[1]/div[1]/div/div[1]/input').send_keys(ticker)
+            sleep(3)
+            browser.find_element_by_xpath('/html/body/div[5]/div[3]/div[1]/div[1]/div/div[2]/table/tbody/tr[1]/td[4]/button').click()
+            sleep(2)
+            price_at_trade_time = StockData(ticker).current_price
+            browser.find_element_by_xpath('/html/body/div[8]/div/div/div[1]/form/div[1]/ul/li[4]').click()
+            browser.find_element_by_xpath('/html/body/div[8]/div/div/div[1]/form/div[2]/div[2]/div[1]/input').send_keys(shares)
+            browser.find_element_by_xpath('/html/body/div[8]/div/div/div[1]/form/div[3]/div/button[3]').click()
+            # Update position in database
+            Database.update_stock_data(ticker, shares * -1, price_at_trade_time)
+            print('Position updated')
+
+        except:
+            print(f'unable to cover {shares} shares of {ticker}')
+
+
+
+def display_portfolio_printout():
+    from texttable import Texttable
+    data = Database.get_json()
+    table = Texttable()
+
+    table.set_cols_align(['c'] * 6)
+    table.set_cols_valign(['m'] * 6)
+    table.add_rows([["stock", "shorted-at", "current-value", "percent-change", "gain-loss", "shares"]])
+    for ticker in data:
+        stock_api_data = StockData(ticker)
+        current_value = stock_api_data.current_price
+        percent_change = f"{round(stock_api_data.percent_change,2)}%"
+        purchased_value = data[ticker]['value']
+        shares = data[ticker]['shares']
+        # also note, since we are only using this bot for shorting stocks, gain and loss are flipped
+        table.add_row([ticker,purchased_value, current_value, percent_change, shares * (purchased_value - current_value),shares ])
+
+    print(table.draw())
+
+display_portfolio_printout()
+
+class BotEngine:
+    @staticmethod
+    def run():
+        while True:
+            # Algorithm for covering current stocks:
+            data = Database.get_json()
+            for ticker in data:
+                stock_api_data = StockData(ticker)
+                percent_change = stock_api_data.percent_change
+                if percent_change < 20:
+                    pass # buy to cover if stock drops 20%
+            # Algorithm to discover new stocks:
+
+            sleep(10)
+    @staticmethod
+    def most_volatile_stocks():
+        pass
